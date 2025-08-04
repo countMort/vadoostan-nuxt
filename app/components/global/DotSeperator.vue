c631c2cc22b3f72cda837e1089ad8c429ea4032b
Morty <gholamimorty@gmail.com>
2025-08-04 17:22:05 +0430

    feat: details page

--- app/components/global/DotSeperator.vue
+++ app/components/global/DotSeperator.vue
@@ -1,12 +1,12 @@
 <template>
   <div
     class="bg-primary rounded-full mx-1.5"
-    :style="{ width: `${size}rem`, height: `${size}rem` }"
+    :style="{ width: `${size / 4}rem`, height: `${size / 4}rem` }"
   />
 </template>
 
 <script setup lang="ts">
-const { size = 0.3125 } = defineProps<{
+const { size = 1.25 } = defineProps<{
   size?: number
 }>()
 </script>
--- app/pages/experiences/[id].vue
+++ app/pages/experiences/[id].vue
@@ -11,7 +11,7 @@
     navigation
   >
     <QCarouselSlide
-      v-for="(photo, index) in experience?.result.expPhotos"
+      v-for="(photo, index) in experience?.expPhotos"
       :key="`experience-photo-${index}`"
       :name="index"
       class="flex justify-center items-center !p-0"
@@ -24,57 +24,105 @@
       />
     </QCarouselSlide>
     <QCarouselSlide
-      v-if="experience?.result.expPhotos.length === 0"
+      v-if="experience?.expPhotos.length === 0"
       :name="0"
       class="flex justify-center items-center"
     >
       <div class="flex flex-col items-center">
-        <q-icon name="image" size="48px" />
+        <QIcon name="image" size="3rem" />
         <p class="text-center">تصویری برای این تجربه موجود نیست</p>
       </div>
     </QCarouselSlide>
 
     <template #navigation-icon="{ active, onClick }">
-      <q-icon
+      <QIcon
         name="horizontal_rule"
         :color="active ? 'primary' : 'grey-4'"
-        size="48px"
+        size="3rem"
         class="mx-1"
         @click="onClick"
       />
     </template>
   </QCarousel>
   <div class="mt-5 px-10">
-    <div class="font-black mb-2.5">شبِ خلق: سفالگری با چرخ</div>
+    <div class="font-black mb-2.5">
+      {{ experience?.title }}
+    </div>
     <div class="flex items-center text-sm">
       <div>۱۲ بهمن، ساعت ۱۶:۰۰</div>
-      <DotSeperator :size="0.625" class="mx-3.5" />
-      <div>محله توحید</div>
+      <DotSeperator :size="2.5" class="mx-3.5" />
+      <div>{{ experience?.address }}</div>
     </div>
     <QSeparator class="!mt-8 !mb-4" />
     <div class="font-black mb-2.5">توضیحات</div>
     <div class="text-sm">
-      در این بازی، شما مثل یک کارآگاه خصوصی با جمع‌آوری سرنخ‌ها؛ هویت قاتل، سلاح
-      مورد استفاده و محل وقوع جرم رو کشف می‌کنید.
+      {{ experience?.description.main }}
     </div>
     <QSeparator class="!mt-8 !mb-4" />
     <div class="font-black mb-2.5">آنچه در تجربه ارائه می‌شود</div>
-    <div class="flex no-wrap">
-      <ShoppingBasket />
+    <div class="flex flex-wrap justify-center gap-2.5">
+      <div
+        v-for="(inclusion, index) in experience?.inclusions"
+        :key="`inclusion-${index}`"
+        class="flex flex-col bg-gray-200 rounded-lg w-20 h-20 items-center justify-center text-sm text-center"
+      >
+        <ShoppingBasket class="mb-1.5" />
+        {{ inclusion }}
+      </div>
     </div>
+    <QSeparator class="!my-3.5" />
+    <div class="font-black mb-2.5">سوالات متداول</div>
+    <div
+      v-for="(faq, index) in experience?.faqs"
+      :key="`faq-${index}`"
+      class="mb-3.5"
+    >
+      <h4 class="mt-1.5 mb-0.5 !text-xs !font-semibold">
+        {{ faq.question }}
+      </h4>
+      <p class="text-xs">
+        {{ faq.answer }}
+      </p>
+    </div>
+    <QSeparator class="!my-3.5" />
+    <QCard
+      v-for="(director, index) in experience?.directors"
+      :key="`director-${index}`"
+      class="!bg-bg-light mb-3.5"
+      flat
+    >
+      <QItem class="!p-0">
+        <QAvatar class="ml-2.5">
+          <NuxtImg
+            :src="baseUrl + director?.photoUrl"
+            :height="48"
+            :width="48"
+          />
+        </QAvatar>
+
+        <QItemSection>
+          <QItemLabel class="font-bold">{{ director?.name }}</QItemLabel>
+          <QItemLabel caption> برگزار کننده </QItemLabel>
+        </QItemSection>
+      </QItem>
+      <p class="text-xs !mt-2.5">
+        {{ director?.bio }}
+      </p>
+    </QCard>
   </div>
 </template>
 
 <script setup lang="ts">
 import { apiKeys, baseUrl } from "~/constants/api.cons"
 import type { ExperienceResponse } from "~/types/api"
-import ShoppingBasket from "~/components/icons/ShoppingBasket.svg"
+import ShoppingBasket from "~/components/icons/ShoppingBasket.vue"
 defineOptions({ name: "ExperiencePage" })
 
 const params = useRoute().params
-const { data: experience } = await useAsyncData<ExperienceResponse>(
-  apiKeys.experience,
-  () => $fetch(`/api/user/experiences/${params.id}`)
+const {
+  data: { value: { result: experience } = {} },
+} = await useAsyncData<ExperienceResponse>(apiKeys.experience, () =>
+  $fetch(`/api/user/experiences/${params.id}`)
 )
 const slide = ref(-1)
 onMounted(() => {
