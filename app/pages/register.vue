01b83d9a31912836acf8d3a660adbc60a8c653e3
Morty <gholamimorty@gmail.com>
2025-08-06 15:40:26 +0430

    [WIP] feat: login and register

--- app/pages/register.vue
+++ app/pages/register.vue
@@ -1,28 +1,40 @@
 <template>
   <div class="flex flex-col gap-3 mt-10 px-3 md:px-8">
-    <QCard>
-      <QCardSection>
-        <QInput v-model="email" label="ایمیل" />
-        <QInput v-model="password" label="رمز عبور" type="password" />
-        <QInput
-          v-model="confirmPassword"
-          label="تایید رمز عبور"
-          type="password"
-        />
-        <QBtn label="ثبت نام" color="primary" @click="register" />
-      </QCardSection>
-    </QCard>
+    <div class="flex gap-3">
+      <QInput v-model="name" label="نام" class="min-w-40 grow" standout />
+      <QInput
+        v-model="surname"
+        label="نام خانوادگی"
+        type="password"
+        standout
+        class="min-w-40 grow"
+      />
+      <QInput
+        v-model="phone"
+        label="شماره تماس"
+        mask="phone"
+        dir="ltr"
+        standout
+        class="min-w-40 grow"
+      />
+      <QBtn
+        label="ثبت نام"
+        color="primary"
+        class="!w-full md:!w-auto mx-auto"
+        @click="register"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 defineOptions({ name: "RegisterPage" })
-const email = ref("")
-const password = ref("")
-const confirmPassword = ref("")
+const name = ref("")
+const surname = ref("")
+const phone = ref("")
 
 const register = () => {
-  console.log(email.value, password.value, confirmPassword.value)
+  console.log(name.value, surname.value, phone.value)
 }
 </script>
 
