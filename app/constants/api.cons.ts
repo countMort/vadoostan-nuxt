0d1837366197c5bb9a1e2fde6a0bdf26068812c9
Morty <gholamimorty@gmail.com>
2025-08-13 15:36:02 +0430

    feat: base url

--- app/constants/api.cons.ts
+++ app/constants/api.cons.ts
@@ -1,5 +1,5 @@
-export const hostName = "141.11.37.39" //  87.107.146.5
-export const baseUrl = "http://" + hostName
+export const hostName = "vasando.ir"
+export const baseUrl = "https://" + hostName
 
 export const apiKeys = {
   experiences: "experiences",
