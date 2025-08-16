1e132f0cbee3df1a4d842d94650dceb02cc6d4c1
Morty <gholamimorty@gmail.com>
2025-08-16 18:46:22 +0430

    fix: pinia cookie

--- app/pages/auth/auth.store.ts
+++ app/pages/auth/auth.store.ts
@@ -1,81 +1,94 @@
 import type { RouteLocationNormalizedGeneric } from "vue-router"
 import { useAuthApi } from "~/api/auth"
 
-export const useAuthStore = defineStore("auth", () => {
-  const token = useCookie<string | null>("token", {
-    default: () => null,
-    sameSite: "none",
-    secure: true,
-    maxAge: 60 * 60 * 24 * 7,
-  })
-  const user = ref<any>(null)
-  const loading = ref(false)
-  const redirect = ref<string | RouteLocationNormalizedGeneric>("/")
-  const error = ref<string | null>(null)
+export const useAuthStore = defineStore(
+  "auth",
+  () => {
+    // const token = useCookie<string | null>("token", {
+    //   default: () => null,
+    //   sameSite: "none",
+    //   secure: true,
+    //   maxAge: 60 * 60 * 24 * 7,
+    // })
+    const token = ref<string | null>(null)
+    const user = ref<any>(null)
+    const loading = ref(false)
+    const redirect = ref<string | RouteLocationNormalizedGeneric>("/")
+    const error = ref<string | null>(null)
 
-  const isAuthenticated = computed(() => !!token.value)
+    const isAuthenticated = computed(() => !!token.value)
 
-  function saveToken(t: string) {
-    token.value = t
-  }
-
-  function clearToken() {
-    token.value = null
-  }
-
-  async function login(mobileNumber: string, otp: string) {
-    loading.value = true
-    error.value = null
-    const { verifyOtp } = useAuthApi()
-    try {
-      const res = await verifyOtp({ mobileNumber, otp, client: "web" })
-      if (!res.result.token) throw new Error("No token returned from server")
-
-      saveToken(res.result.token)
-      const user = await fetchUser()
-      if (!user) throw new Error()
-      navigateTo(redirect.value)
-      redirect.value = "/"
-    } catch (err: any) {
-      error.value = err?.data?.message || err.message || "خطایی رخ داد"
-      console.log("login error: ", error.value)
-    } finally {
-      loading.value = false
+    function saveToken(t: string) {
+      token.value = t
     }
-  }
 
-  function logout() {
-    clearToken()
-    user.value = null
-    navigateTo("/login")
-  }
+    function clearToken() {
+      token.value = null
+    }
 
-  async function fetchUser() {
-    if (!token.value) return
-    loading.value = true
-    const { fetchUser: fetchUserApi } = useAuthApi()
-    try {
-      const res = await fetchUserApi()
-      user.value = res.result
-      return res.result
-    } catch (err: any) {
-      console.log("fetchUser error: ", err)
+    async function login(mobileNumber: string, otp: string) {
+      loading.value = true
+      error.value = null
+      const { verifyOtp } = useAuthApi()
+      try {
+        const res = await verifyOtp({ mobileNumber, otp, client: "web" })
+        if (!res.result.token) throw new Error("No token returned from server")
+
+        saveToken(res.result.token)
+        const user = await fetchUser()
+        if (!user) throw new Error()
+        navigateTo(redirect.value)
+        redirect.value = "/"
+      } catch (err: any) {
+        error.value = err?.data?.message || err.message || "خطایی رخ داد"
+        console.log("login error: ", error.value)
+      } finally {
+        loading.value = false
+      }
+    }
+
+    function logout() {
       clearToken()
       user.value = null
-    } finally {
-      loading.value = false
+      navigateTo("/login")
     }
-  }
 
-  return {
-    token,
-    user,
-    loading,
-    error,
-    isAuthenticated,
-    login,
-    logout,
-    fetchUser,
-    redirect,
+    async function fetchUser() {
+      if (!token.value) return
+      loading.value = true
+      const { fetchUser: fetchUserApi } = useAuthApi()
+      try {
+        const res = await fetchUserApi()
+        user.value = res.result
+        return res.result
+      } catch (err: any) {
+        console.log("fetchUser error: ", err)
+        clearToken()
+        user.value = null
+      } finally {
+        loading.value = false
+      }
+    }
+
+    return {
+      token,
+      user,
+      loading,
+      error,
+      isAuthenticated,
+      login,
+      logout,
+      fetchUser,
+      redirect,
+    }
+  },
+  {
+    persist: {
+      storage: piniaPluginPersistedstate.cookies({
+        sameSite: "none",
+        secure: true,
+      }),
+      pick: ["token", "user", "redirect"],
+    },
   }
-})
+)
