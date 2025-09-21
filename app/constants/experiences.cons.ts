82d59208251390a20803df50dae2de5677da9e25
Morty <gholamimorty@gmail.com>
2025-09-21 15:04:55 +0430

    feat: exp card categories

--- app/constants/experiences.cons.ts
+++ app/constants/experiences.cons.ts
@@ -13,6 +13,54 @@ export const experienceCategories = {
     label: "علمی",
     color: "#AEFBDC",
   },
+  game: {
+    label: "بازی",
+    color: "#FFD497",
+  },
+  handicraft: {
+    label: "خلق",
+    color: "#A1C8F6",
+  },
+  cooking: {
+    label: "آشپزی",
+    color: "#6DE69F",
+  },
+  talk: {
+    label: "گفتگو",
+    color: "#EF9CCA",
+  },
+  trip: {
+    label: "کوله‌بار",
+    color: "#9DF0F0",
+  },
+  performing_arts: {
+    label: "هنرهای نمایشی",
+    color: "#A1C8F6",
+  },
+  homa_and_hoom: {
+    label: "هماوهوم",
+    color: "#AEFBDC",
+  },
+  va_tan: {
+    label: "و‌تن",
+    color: "#EFEC9C ",
+  },
+  fantasy: {
+    label: "فانتزی",
+    color: "#A1C8F6",
+  },
+  nations: {
+    label: "ملل",
+    color: "#A1C8F6",
+  },
+  literature: {
+    label: "ادبیات",
+    color: "#A1C8F6",
+  },
+  language: {
+    label: "زبان",
+    color: "#A1C8F6",
+  },
 }
 
 export const statusDic: Record<
